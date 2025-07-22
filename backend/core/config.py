45a8a1082679aa3474a4ac4d0bd38ed7f103b719
Vladeptres <feahmpo.92@gmail.com>
2025-07-23 01:45:17 +0200

    s3(credentials): set up s3 credentials env var in constants

--- backend/core/config.py
+++ backend/core/config.py
@@ -8,3 +8,11 @@ MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "bourracho_db_dev")
 CONVERSATIONS_COLLECTION = "conversations"
 USERS_COLLECTION = "users"
 MESSAGES_COLLECTION = "messages"
+MEDIA_STORAGE_PATH = os.environ.get("MEDIA_STORAGE_PATH", "media_files")
+
+
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "bourracho-test")
+S3_REGION = "eu-north-1"
+S3_ENDPOINT_URL = "https://s3.eu-north-1.amazonaws.com"
+S3_ACCESS_KEY_ID = os.environ.get("S3_ACCESS_KEY_ID", "")
+S3_SECRET_ACCESS_KEY = os.environ.get("S3_SECRET_ACCESS_KEY", "")
