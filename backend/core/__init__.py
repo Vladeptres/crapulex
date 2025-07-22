8efb7ef022d0194ab0468f97378cfdfd6131d60f
Vladeptres <feahmpo.92@gmail.com>
2025-07-23 01:45:38 +0200

    core(medias): handle medias on S3 object storage

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,10 +1,11 @@
 __all__ = [
     "config",
     "conversations_store",
+    "medias_store",
     "messages_store",
     "models",
     "stores_registry",
     "users_store",
 ]
 
-from core import config, conversations_store, messages_store, models, stores_registry, users_store
+from core import config, conversations_store, medias_store, messages_store, models, stores_registry, users_store
