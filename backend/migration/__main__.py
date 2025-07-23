31c6488285bcffdb2dfcd4fb73d6aa4f82183ee4
Vladeptres <feahmpo.92@gmail.com>
2025-07-23 11:54:37 +0200

    migration(db): fix DB migration script

--- backend/migration/__main__.py
+++ backend/migration/__main__.py
@@ -4,4 +4,4 @@ import os
 MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "bourracho_api_test")
 
 if __name__ == "__main__":
-    db.update_conversations_collections(from_db=MONGO_DB_NAME, to_db=MONGO_DB_NAME + "_0_2")
+    db.update_conversations_collections(from_db=MONGO_DB_NAME, to_db=MONGO_DB_NAME + "_0_2b")
--- backend/migration/db.py
+++ backend/migration/db.py
@@ -1,30 +1,35 @@
 from pymongo import MongoClient
-from core import config as cf
-
 from loguru import logger
+import os
 
+MONGO_DB_URL = os.environ.get("MONGO_DB_URL", "mongodb://localhost:27017")
 def update_conversations_collections(from_db: str, to_db: str):
     try:
-        client = MongoClient(cf.MONGO_DB_URL)
+        client = MongoClient(MONGO_DB_URL)
         old_db = client[from_db]
         new_db = client[to_db]
-        conversations_collection = old_db[cf.CONVERSATIONS_COLLECTION]
+        conversations_collection = old_db["conversations"]
         for conversation in conversations_collection.find():
             logger.info("Updating conversation " + str(conversation["name"]))
             conversation["is_visible"] = True
-            new_db[cf.CONVERSATIONS_COLLECTION].insert_one(conversation)
+            if conversation["users_ids"]:
+                conversation["admin_id"] = conversation["users_ids"][0]
+            else:
+                conversation["admin_id"] = ""
+            new_db["conversations"].insert_one(conversation)
 
         logger.success("Finished converting all discussions.")
         logger.info("Copying Users")
-        users_collection = old_db[cf.USERS_COLLECTION]
+        users_collection = old_db["users"]
         for user in users_collection.find():
             logger.info("Updating user " + str(user["username"]))
-            new_db[cf.USERS_COLLECTION].insert_one(user)
+            new_db["users"].insert_one(user)
         logger.success("Finished converting all users.")
-        messages_collection = old_db[cf.MESSAGES_COLLECTION]
+        messages_collection = old_db["messages"]
         for message in messages_collection.find():
             logger.info("Updating message " + str(message["id"]))
-            new_db[cf.MESSAGES_COLLECTION].insert_one(message)
+            message["medias_metadas"] = []
+            new_db["messages"].insert_one(message)
         logger.success("Finished converting all messages.")
         client.close()
     except Exception as e:
