29e4b5d8ed8e6c9e3fc84f08bcc7a874f05d4b81
Vladeptres <feahmpo.92@gmail.com>
2025-09-09 03:22:45 +0200

    frontend(deps): update frontend dependencies to remove security issues

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.utk1cihojco"
+    "revision": "0.t2nmv95rlq4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
